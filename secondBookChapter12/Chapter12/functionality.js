c53caa506ad49a6b15f177744b480e74b9ca4836
J4d4-M4thele <102987102+J4d4-M4thele@users.noreply.github.com>
2023-04-27 09:44:30 +0200

    Add files via upload

---
+++ secondBookChapter12/Chapter12/functionality.js
@@ -0,0 +1,42 @@
+let trainSpeed = 250;
+let trainPosition = 0;
+let animation;
+
+let train = document.getElementById("train");
+train.addEventListener("click", speedUp);
+
+let stopButton = document.getElementById("stopButton");
+stopButton.addEventListener("click", stopTrain);
+
+function speedUp() {
+    if (trainSpeed > 10) {
+        trainSpeed -= 10;
+    }
+    console.log("train speed: " + trainSpeed);
+
+    clearInterval(animation);
+    animation = setInterval(frame, trainSpeed);
+
+
+    function frame() {
+        trainPosition += 2;
+        train.style.left = trainPosition + 'px';
+        console.log(trainPosition);
+        checkPosition(trainPosition);
+    }
+}
+//lengthen track by changing the position.
+function checkPosition(currentPosition) {
+    if (currentPosition === 600) {
+        alert(":O!");
+        console.log("Crash!");
+        clearInterval(animation);
+    }
+}
+
+function stopTrain() {
+    if (trainPosition < 600) {
+        clearInterval(animation);
+        console.log("Whew! That was close!");
+    }
+}
